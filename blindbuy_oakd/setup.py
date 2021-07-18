af4e7575750a8a18b22d0c497d0c80dff09ab85a
DaniGarciaLopez <d.garcialopez@hotmail.com>
2021-07-18 16:41:56 +0200

    Changed to python package

---
+++ blindbuy_oakd/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'blindbuy_oakd'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'urdf','models'), glob('urdf/models/*.dae')),
+        
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dani',
+    maintainer_email='d.garcialopez@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
